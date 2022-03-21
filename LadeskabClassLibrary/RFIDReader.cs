453b8b05a9b32f31695c4cce670a7c185b4c8fe3
hundahl18 <hundahl18@gmail.com>
2022-03-21 20:36:00 +0100

    Made RFIDEventarg but in wrong place

--- LadeskabClassLibrary/RFIDReader.cs
+++ LadeskabClassLibrary/RFIDReader.cs
@@ -6,7 +6,14 @@ using System.Threading.Tasks;
 
 namespace LadeskabClassLibrary
 {
+    public class RFIDDetectedEventArgs : EventArgs
+    {
+        //RFID ID as int
+        public int ID { set; get; }
+    }
     public class RFIDReader: IRFIDReaders
     {
+        // Event triggered on new ID value
+        event EventHandler<CurrentEventArgs> CurrentValueEvent;
     }
 }
