d8d0b8174db1d8420a96e2de04a18d3ce1318844
Rasmus Munk Engelund <au683445@uni.au.dk>
2022-03-22 11:22:30 +0100

    Door Eventargs

--- LadeskabClassLibrary/Door.cs
+++ LadeskabClassLibrary/Door.cs
@@ -6,33 +6,50 @@ using System.Threading.Tasks;
 
 namespace LadeskabClassLibrary
 {
-
+  
     public class Door : IDoor
     {
+        public event EventHandler<DoorStateChangedEventArgs> DoorStateChanged;
+
+        public bool State { set; get; }
+
         public Door()
-        { }
+        {
+            State = true;
+        }
 
         void IDoor.LockDoor()
         {
-
+            if (!State)
+            {
+                OnDoorStateChanged(new DoorStateChangedEventArgs { _DoorOpen = false });
+            }
+            else
+            {
+                throw new ArgumentOutOfRangeException();
+            }
         }
 
         void IDoor.UnlockDoor()
         {
-
+            if (State)
+            {
+                OnDoorStateChanged(new DoorStateChangedEventArgs { _DoorOpen = true });
+            }
+            else
+            {
+                throw new ArgumentOutOfRangeException();
+            }
         }
 
-        public void OnDoorStateChanged()
-        { 
-        }
-
-        void IDoor.SetDoorState(bool DoorOpen)
+        private void OnDoorStateChanged(DoorStateChangedEventArgs e)
         {
-        
+            DoorStateChanged?.Invoke(this, new DoorStateChangedEventArgs() { _DoorOpen = this.State });
         }
 
 
-        public event EventHandler<DoorStateChangedEventArgs> DoorStateChangedEventArgs;
+
+        
 
     }
 }
