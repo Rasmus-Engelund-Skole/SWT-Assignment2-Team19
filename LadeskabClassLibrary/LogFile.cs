d2ae5b1dd0717a72424c3e0dfb7b72d5a1d5d3e6
Rasmus Munk Engelund <au683445@uni.au.dk>
2022-03-23 20:01:01 +0100

    log

--- LadeskabClassLibrary/LogFile.cs
+++ LadeskabClassLibrary/LogFile.cs
@@ -9,11 +9,42 @@ namespace LadeskabClassLibrary
 {
     public class Logfile : ILogfile
     {
+        public int LogId { get; set; }
+
         public string LogFile { get; set; }
 
-        public StreamWriter AppText(string logfile)
+        public Logfile()
         {
-            return File.AppendText(logfile);
+            LogId = 0;  
+        }
+
+        public void LogText(string path, string Log)
+        {
+            LogFile = path;
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.Append("Id: " + LogId);
+            stringBuilder.Append("Log time: " + DateTime.Now);
+            stringBuilder.Append("Message: " + Log + "\n");
+            File.AppendAllText(LogFile, stringBuilder.ToString());
+            stringBuilder.Clear();
+
+            LogId++;
+        }
+
+
+        public void FakeAddLogEntry(string message, List<string> logList)
+        {
+            // Metoden er en fake og er lavet for testing-purpose
+
+            StringBuilder stringBuilder = new StringBuilder();
+
+            stringBuilder.Append("Id: " + LogId + " - ");
+            stringBuilder.Append("Log time: " + DateTime.Now + " - ");
+            stringBuilder.Append("Message: " + message + "\n");
+
+            logList.Add(stringBuilder.ToString());
+
+            LogId++;
         }
     }
 }
