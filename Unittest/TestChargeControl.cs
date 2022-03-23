fe4c36046373351307cc6387a558f3df62e07290
odennis2 <oscard@live.dk>
2022-03-23 20:47:09 +0100

    charge control test startet

---
+++ Unittest/TestChargeControl.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace LadeskabClassLibrary
+{
+    public class TestChargeControl
+    {
+        private ChargeControl _uut;
+        private IDisplay _fakeDisplay;
+        private IUsbCharger _fakeUsbCharger;
+
+
+        [SetUp]
+        public void SetUp()
+        {
+            _fakeDisplay = Substitute.For<IDisplay>();
+            _fakeUsbCharger = Substitute.For<IUsbCharger>();
+
+            _uut = new ChargeControl(_fakeUsbCharger, _fakeDisplay);
+        }
+
+        [Test]
+        public void StartCharge()
+        {
+
+        }
+
+        [Test]
+        public void StopCharge()
+        {
+
+        }
+
+    }
+}
